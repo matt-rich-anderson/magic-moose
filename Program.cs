1d83a2b4ee6d7e987d61f72ba16c6e293574d6d2
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-02-24 15:44:50 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+    Console.WriteLine("Ask your question quickly, Magic Moose is busy.");
+    Console.WriteLine("-----------------------------------------------");
+
+    MoosePredicts()
+
+}
+
+void MoosePredicts()
+{
+    string answer = Console.ReadLine().ToLower();
+    if()
+}
+
+//program need to determine if weather somthing was input or not
+//if not, program terminates
+//if input provided, program generates random number
+//generated number is matched with a designated prediction string
+// matched string is output
+//program ends
