69f8beb55423864002b69a30549b3bb6397b6f39
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-02-25 08:29:50 -0600

    completed program

--- Program.cs
+++ Program.cs
@@ -1,25 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 Main();
 
 void Main()
 {
+    Console.WriteLine("-----------------------------------------------");
     Console.WriteLine("Ask your question quickly, Magic Moose is busy.");
     Console.WriteLine("-----------------------------------------------");
 
-    MoosePredicts()
-
+    MoosePredicts();
 }
 
 void MoosePredicts()
 {
+    List<string> possiblePredictions = new List<string>() 
+    { 
+        "As I see it, yes.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "It is certain.",
+        "It is decidedly so.",
+        "Most likely.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Outlook good.",
+        "Reply hazy, try again.",
+        "Signs point to yes.",
+        "Very doubtful.",
+        "Without a doubt.",
+        "Yes.",
+        "Yes - definitely.",
+        "You may rely on it." 
+    };
+
+    Console.Write($"Ask your question:");
     string answer = Console.ReadLine().ToLower();
-    if()
+    
+    if(string.IsNullOrEmpty(answer))
+    {
+        Environment.Exit(0);
+    }
+    else
+    {
+        Random r = new Random();
+        int rando= r.Next(0,18);
+        string spiritMessage = possiblePredictions[rando];
+        Console.Write($"The spirits tell me... '{spiritMessage}'");        
+        Environment.Exit(0);
+    }
 }
 
-//program need to determine if weather somthing was input or not
-//if not, program terminates
-//if input provided, program generates random number
-//generated number is matched with a designated prediction string
-// matched string is output
-//program ends
+
